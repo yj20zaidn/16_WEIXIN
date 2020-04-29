2c1d483621346ce929ded699dfa361381a9bdb6b
yaoj <729365885@qq.com>
2020-04-29 19:19:18 +0800

    提交WEIXIN二次

---
+++ day_02/myproject01/pages/p04/p04.js
@@ -0,0 +1,76 @@
+// pages/p04/p04.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    ename: 'liangliang',
+    age: 80.5,
+    isMarried: false,
+    boss: {bname:'大壮',age:50},
+
+    avatar: '/assets/images/2.jpg',
+    photo: '3.jpg',
+    myStyle02: 'color:green;',
+    myStyle03: 'blue',
+    myClass04: 'danger'
+    //myClass04: 'success'
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
